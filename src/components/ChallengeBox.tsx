8638eac5eecf3a38dabf8b584c2f9c9735727093
bezerrarodrigo <bezerra.rodrigo@gmail.com>
2021-02-24 22:49:22 -0300

    Adicioanado componente de ChallengeActive

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,41 @@
-import styles from '../styles/components/ChallengeBox.module.css'
+import styles from "../styles/components/ChallengeBox.module.css";
 
 export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
   return (
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
-        <strong>Finalize um ciclo para receber um desafio</strong>
-        <p>
-          <img src="./icons/level-up.svg" alt="Level Up" />
-          Avance de levels completando desafios.
-        </p>
-      </div>
+      {hasActiveChallenge ? (
+
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+          <main>
+            <img src="icons/body.svg" alt="level Up Active" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+          <footer className={styles.activeChallengeButtons}>
+            <button
+              type="button"
+              className={styles.challengeFailButton}>
+              Falhei
+              </button>
+            <button
+              type="button"
+              className={styles.challengeSuccessButton}>
+              Completei
+              </button>
+          </footer>
+        </div>
+      ) : (
+          <div className={styles.challengeNotActive}>
+            <strong>Finalize um ciclo para receber um desafio</strong>
+            <p>
+              <img src="./icons/level-up.svg" alt="Level Up" />
+            Avance de levels completando desafios.
+          </p>
+          </div>
+        )}
     </div>
-  )
+  );
 }
